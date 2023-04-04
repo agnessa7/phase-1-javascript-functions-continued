95333a3df56d08087417d987840a9dd832022a10
Agnessa Nikolayev <anikolayev90@gmail.com>
2023-04-04 14:54:05 -0700

    adding flair

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+
+
+function saturdayFun( activity = "roller-skate") {
+
+
+    return `This Saturday, I want to ${activity}!`
+} 
+
+function mondayWork( activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+ function wrapAdjective(value = "*"){
+   
+    const popconInside = function ( butter = "special" ){
+        return `You are ${value}${butter}${value}!`;
+    };
+    return popconInside;
+ }
